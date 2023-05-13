625ab9994b8dc527edfad3da8304ad13592fbc19
daindian <105160068+daindian@users.noreply.github.com>
2023-05-14 00:33:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '叶霜琳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oL_qg6jb4DJImuAu-F2Z-Ctk_KmY',
+      id: 'oL_qg6lazA6FPFO-aE29XLXG3knI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Wwx1chUYhl6CUTYEDouhrm-ZueQNlfJaKYc1tFxi0gI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2023', date: '04-10',
+          type: '*生日', name: '宝贝', year: '2004', date: '10-19',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2023', date: '04-10',
+          type: '生日', name: '李四', year: '2004', date: '10-19',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '04-10',
