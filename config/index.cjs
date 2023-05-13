781251cb21cbfc14369455aab9ff61f7f15816f8
daindian <105160068+daindian@users.noreply.github.com>
2023-05-14 00:17:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oL_qg6jb4DJImuAu-F2Z-Ctk_KmY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	Wwx1chUYhl6CUTYEDouhrm-ZueQNlfJaKYc1tFxi0gI',
+      useTemplateId: 'RwQG13QEtd9QykaOdB58exoD_Pkb1Cv8TDaGzSz6oY0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
