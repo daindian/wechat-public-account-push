0654b9ff661a5c8dd8a08042238c16e98bccc74d
daindian <105160068+daindian@users.noreply.github.com>
2023-05-14 13:35:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '叶宝', year: '2004', date: '10-19',
+          type: '*生日', name: '叶宝', year: '2004', date: '09-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '叶宝', year: '2004', date: '09-06',
+          type: '生日', name: '叶宝', year: '2004', date: '10-19',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '04-10',
